56c37055918be03d045bf5000c623d90e325aac9
shpaw415 <shpaw415@gmail.com>
2023-11-01 13:57:33 -0400

    add config exemple

---
+++ config.exemple.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ENCRYPTION_KEY', '__CUSTOM_ENCYPTION_KEY__'); // Ex: u84321&^%^%$#^%$#
+define('ENCRYPTION_IV', '__CUSTOM_ENCRYPTION_IV'); // Ex: 1234567890123456
+
+
+# GoogleApi
+define('GOOGLE_API_KEY', '__GOOGLE_API_KEY__'); // set to your Google API key
+###
+
+# PaypalApi
+define('PAYPAL_CLIENT_ID', '__PAYPAL_CLIENT_ID__'); // set to your PayPal client ID
+define('PAYPAL_CLIENT_SECRET', '__PAYPAL_CLIENT_SECRET__'); // set to your PayPal client secret
+define('PAYPAL_API_HOST', 'https://www.sandbox.paypal.com'); // set to your PayPal API host sandbox or production
+###
